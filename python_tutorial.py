9201d951128752015a6f46ab256dc22989bd6fa6
UD_resh <reshmitha.0424@gmail.com>
2026-02-26 14:43:32 -0500

    Day 2 python tutorial

--- python_tutorial.py
+++ python_tutorial.py
@@ -127,5 +127,22 @@ myfunc2()
 print("Python is "+ y)
 print("Python is "+ x)
 
+##########################################
+# -------PYTHON DATA TYPES--------
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+##########################################
+# -------PYTHON NUMBERS--------
+# int, float, complex, random number
+import random
+
+print(random.randrange(1, 10))
 
 
